6bed2fab0fca449a6d91e386e4ab89272e3ab1a5
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2024-07-07 10:26:46 +0530

    Editor Page

--- realtime-editor/src/pages/EditorPage.js
+++ realtime-editor/src/pages/EditorPage.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+import {initSocket} from "../socket";
+import {useLocation} from "react-router-dom";
 
 const EditorPage = () => {
-    return <div className="homePageWrapper">
-        <div className="formWrapper"></div>
+
+    const socketRef = useRef(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const init = async () => {
+           socketRef.current = await initSocket();
+           socketRef.current.emit(ACTIONS.JOIN, {
+            roomId,
+            username: location.state?.username,
+           });
+        }
+
+        init();
+
+    }, []);
+
+    const [clients] = useState([
+        {socketId : 1, username : "krishna"},
+        {socketId : 2, username : "John"},
+        {socketId : 3, username : "vishna"},
+        {socketId : 4, username : "Rohaan"} ]);
+
+    return( 
+     <div className="mainWrap">
+        <div className="aside">
+            <div className="asideInner">
+                <div className="logo">
+                    <img className="logoImage" src="/c (2) (1).png" alt="logo"/>
+                </div>
+
+                <h3>Connected</h3>
+
+                <div className="clientsList">
+                   {clients.map((client) => (
+                    <Client 
+                    key = {client.socketId}
+                    username = {client.username}/>
+                   ))}
+                </div>
+
+            </div>
+
+            <button className="btn copyBtn">Copy Room ID</button>
+            <button className="btn leaveBtn">Leave</button>
+        </div>
+
+        <div className="editorWrap">
+            <Editor/>
+        </div>
+
     </div>
-};
+    );
+}
 
 export default EditorPage;
--- realtime-editor/src/pages/Home.js
+++ realtime-editor/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import {v4 as uuidv4} from "uuid";
 import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
+
 const Home = () => {
 
+    const navigate = useNavigate();
+
     const [roomId, setRoomId] = useState("");
-    const [userName, setUserName] = useState("");
+
+    const [username, setUsername] = useState("");
+
     const createNewRoom = (e) =>{
       e.preventDefault();
 
@@ -15,9 +21,28 @@ const Home = () => {
       toast.success("Created a new room");
     };
 
+    const joinRoom = () =>{
+        if(!roomId || !username){
+            toast.error("Room ID & Username is required");
+            return;
+        }
+
+        navigate("/editor/" + {roomId},{
+            state : {
+              username,
+            }
+        })
+    };
+
+    const handleInputEnter = (e) => {
+      if(e.code === "Enter"){
+        joinRoom();
+      }
+    }
+
     return <div className="homePageWrapper">
     <div className="formWrapper">
-        <img src="/c (1).png" alt="code-logo" className="homePageLogo"/>
+        <img src="/c (2) (1).png" alt="code-logo" className="homePageLogo"/>
 
         <h4 className="mainLable">Paste Invitation Room Id</h4>
 
@@ -25,13 +50,17 @@ const Home = () => {
 
             <input type="text" className="inputBox" placeholder="ROOM ID"
              onChange = {(e) => setRoomId(e.target.value)}
-             value={roomId}/>
+             value={roomId}
+             onKeyUp={handleInputEnter}
+             />
 
             <input type="text" className="inputBox" placeholder="USERNAME" 
-             onChange = {(e) => setUserName(e.target.value)}
-             value={userName}/>
+             onChange = {(e) => setUsername(e.target.value)}
+             value={username}
+             onKeyUp={handleInputEnter}
+             />
 
-            <button className="btn joinBtn">Join</button>
+            <button className="btn joinBtn" onClick={joinRoom}>Join</button>
 
             <span className="createInfo">If you don't have an invite then create &nbsp;
             <a onClick={createNewRoom} href=""className="createNewBtn">new room</a></span>
